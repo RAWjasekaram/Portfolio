eef528ddef7f0978f517c2e371903f9f522a058f
RAWjasekaram <rishna.rajasekaram@gmail.com>
2023-07-29 13:23:38 -0400

    test

--- Rishna/src/components/Header.tsx
+++ Rishna/src/components/Header.tsx
@@ -4,7 +4,7 @@ const Header: React.FC = () => {
   return (
     <header className="bg-gray-800 text-white p-4">
       <div className="container mx-auto">
-        <h1 className="text-3xl font-bold">Your Name</h1>
+        <h1 className="text-3xl font-bold">Rishna Rajasekaram</h1>
         <p className="text-xl">Frontend Developer</p>
       </div>
     </header>
