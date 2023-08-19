89584d6a32de70d9d77aed7c92417244558d2722
RAWjasekaram <rishna.rajasekaram@gmail.com>
2023-08-19 12:23:09 -0400

    add form.

--- with-tailwindcss-app/app/page.tsx
+++ with-tailwindcss-app/app/page.tsx
@@ -4,6 +4,7 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
+import Email from "./components/Email";
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
       <Skills />
       <Projects />
       <Contact />
+      <Email />
     </div>
   );
 }
