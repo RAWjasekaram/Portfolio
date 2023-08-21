636e7bb113173a4229501f8e2c11e451f0d577a8
RAWjasekaram <rishna.rajasekaram@gmail.com>
2023-08-21 16:13:46 -0400

    update to skills section

--- with-tailwindcss-app/app/components/Email.tsx
+++ with-tailwindcss-app/app/components/Email.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Email: React.FC = () => {
   return (
-    <div className="flex justify-center w-full">
+    <div className="flex justify-center w-full mx-auto">
       <form
         className="w-full"
         action="https://getform.io/f/f0b8d807-73a4-4d90-af94-fa134e93610c"
@@ -10,7 +10,7 @@ const Email: React.FC = () => {
       >
         <input
           required
-          className="my-8"
+          className="my-8 mx-auto"
           placeholder="Name"
           type="text"
           name="name"
@@ -18,7 +18,7 @@ const Email: React.FC = () => {
         <br />
         <input
           required
-          className="my-4"
+          className="my-4 mx-auto"
           placeholder="E-mail"
           type="email"
           name="email"
@@ -26,12 +26,12 @@ const Email: React.FC = () => {
         <br />
         <input
           required
-          className="my-4 w-1/2 h-260px"
+          className="my-4 w-1/2 h-280px"
           placeholder="Message"
           type="text"
           name="message"
         />
-
+        <br />
         <button type="submit">Send</button>
       </form>
     </div>
--- with-tailwindcss-app/app/components/Skills.tsx
+++ with-tailwindcss-app/app/components/Skills.tsx
@@ -2,14 +2,49 @@ import React from "react";
 
 const Skills: React.FC = () => {
   return (
-    <div className="container mx-auto">
+    <div className="container mx-auto  justify-center">
       {" "}
-      <h2 className="text-3xl font-bold underline text-yellow-700" id="2">
+      <h2
+        className="text-3xl font-bold underline text-yellow-700 flex justify-center"
+        id="2"
+      >
         Skills
-        <br />
-        <br />
-        <br />
-      </h2>{" "}
+      </h2>
+      <div className="flex justify-center bg-white">
+        <div className="flex space-x-16">
+          <img
+            src="https://brandeps.com/logo-download/C/CSS-3-logo-vector-01.svg"
+            className="w-24 h-32  mx-auto md:mx-0 mb-4 md:mb-0"
+          />
+          <img
+            src="https://brandeps.com/logo-download/T/Typescript-logo-vector-02.svg"
+            className="w-24 h-32  mx-auto md:mx-0 mb-4 md:mb-0"
+          />
+          <img
+            src="https://brandeps.com/logo-download/J/JavaScript-logo-vector-01.svg"
+            className="w-24 h-32  mx-auto md:mx-0 mb-4 md:mb-0"
+          />
+          <img
+            src="https://brandeps.com/logo-download/H/HTML-5-logo-vector-01.svg"
+            className="w-24 h-32  mx-auto md:mx-0 mb-4 md:mb-0"
+          />
+          <img
+            src="https://brandeps.com/icon-download/G/Github-icon-vector-22.svg"
+            className="w-24 h-32  mx-auto md:mx-0 mb-4 md:mb-0"
+          />
+          <img
+            src="https://brandeps.com/icon-download/N/Nextjs-icon-vector-01.svg"
+            className="w-24 h-32  mx-auto md:mx-0 mb-4 md:mb-0"
+          />
+          <img
+            src="https://brandeps.com/logo-download/R/React-logo-vector-01.svg"
+            className="w-24 h-32  mx-auto md:mx-0 mb-4 md:mb-0"
+          />
+        </div>
+      </div>
+      <br />
+      <br />
+      <br />{" "}
     </div>
   );
 
