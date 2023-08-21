d578a189a66577fdd79fb27734428ae34beae99a
RAWjasekaram <rishna.rajasekaram@gmail.com>
2023-08-21 16:30:47 -0400

    update contact

--- with-tailwindcss-app/app/components/Email.tsx
+++ with-tailwindcss-app/app/components/Email.tsx
@@ -2,37 +2,39 @@ import React from "react";
 
 const Email: React.FC = () => {
   return (
-    <div className="flex justify-center w-full mx-auto">
+    <div className="mx-auto">
       <form
-        className="w-full"
+        className=""
         action="https://getform.io/f/f0b8d807-73a4-4d90-af94-fa134e93610c"
         method="POST"
       >
-        <input
-          required
-          className="my-8 mx-auto"
-          placeholder="Name"
-          type="text"
-          name="name"
-        />
-        <br />
-        <input
-          required
-          className="my-4 mx-auto"
-          placeholder="E-mail"
-          type="email"
-          name="email"
-        />
-        <br />
-        <input
-          required
-          className="my-4 w-1/2 h-280px"
-          placeholder="Message"
-          type="text"
-          name="message"
-        />
-        <br />
-        <button type="submit">Send</button>
+        <div className=" flex flex-col">
+          <input
+            required
+            className=""
+            placeholder="Name"
+            type="text"
+            name="name"
+          />
+
+          <input
+            required
+            className=""
+            placeholder="E-mail"
+            type="email"
+            name="email"
+          />
+
+          <input
+            required
+            className=""
+            placeholder="Message"
+            type="text"
+            name="message"
+          />
+          <br />
+          <button type="submit">Send</button>
+        </div>
       </form>
     </div>
   );
