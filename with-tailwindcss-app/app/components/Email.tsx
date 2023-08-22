fb8ab6e0ac6964136863a1274c027240842d2666
RAWjasekaram <rishna.rajasekaram@gmail.com>
2023-08-22 00:51:00 -0400

    made form work for the most part
    Still an issue with the box extended infinitely and not using up the space allotted

--- with-tailwindcss-app/app/components/Email.tsx
+++ with-tailwindcss-app/app/components/Email.tsx
@@ -2,16 +2,16 @@ import React from "react";
 
 const Email: React.FC = () => {
   return (
-    <div className="mx-auto">
+    <div className="flex flex-col text-xl mx-4 lg:mx-80">
       <form
         className=""
         action="https://getform.io/f/f0b8d807-73a4-4d90-af94-fa134e93610c"
         method="POST"
       >
-        <div className=" flex flex-col">
+        <div className=" flex flex-col mx-24 space-y-8">
           <input
             required
-            className=""
+            className="w-1/3 mx-auto"
             placeholder="Name"
             type="text"
             name="name"
@@ -19,7 +19,7 @@ const Email: React.FC = () => {
 
           <input
             required
-            className=""
+            className=" w-1/3 mx-auto"
             placeholder="E-mail"
             type="email"
             name="email"
@@ -27,7 +27,7 @@ const Email: React.FC = () => {
 
           <input
             required
-            className=""
+            className="w-4/5 mx-auto h-40" //Message box extends infinitely despite size? just acts as one line like notepad and extends 4ever//
             placeholder="Message"
             type="text"
             name="message"
